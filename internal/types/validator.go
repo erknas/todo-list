065ea89063bd0c3b2836a5d601443bb22119213e
Aleksandr Vinogradov <vin89441@gmail.com>
2025-02-28 15:48:53 +0300

    add validate on create task

--- internal/types/validator.go
+++ internal/types/validator.go
@@ -2,7 +2,17 @@ package types
 
 import "github.com/gofiber/fiber/v3"
 
-func (t TaskRequest) ValidateTaskRequest() fiber.Map {
+func (t TaskRequest) ValidateCreateTaskRequest() fiber.Map {
+	errors := make(fiber.Map)
+
+	if t.Status != "new" && t.Status != "in_progress" && t.Status != "done" {
+		errors["status"] = "wrong status"
+	}
+
+	return errors
+}
+
+func (t TaskRequest) ValidateUpdateTaskRequest() fiber.Map {
 	errors := make(fiber.Map)
 
 	if len(t.Status) == 0 {
