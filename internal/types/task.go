589fdca1210f96940c36c88a298c91f0ec9d5b01
Aleksandr Vinogradov <vin89441@gmail.com>
2025-03-01 12:59:05 +0300

    add task response

--- internal/types/task.go
+++ internal/types/task.go
@@ -17,8 +17,21 @@ type Tasks struct {
 	Tasks []Task `json:"tasks"`
 }
 
-type TaskRequest struct {
+type NewTaskRequest struct {
 	Title       string `json:"title"`
 	Description string `json:"description"`
 	Status      string `json:"status"`
+}
+
+type NewTaskResponse struct {
+	ID          int       `json:"id"`
+	Title       string    `json:"title"`
+	Description string    `json:"description"`
+	Status      string    `json:"status"`
+	CreatedAt   time.Time `json:"createdAt"`
+}
+
+type TaskResp struct {
+	TaskID int    `json:"taskID"`
+	Msg    string `json:"msg"`
 }
